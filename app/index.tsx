2077f311554da76e9fe3d3cbdb9d9b9507ade77a
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-09-11 11:09:58 +0200

    add image

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { ImageBackground, Text, View } from 'react-native';
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import beachImage from '@/assets/meditation-images/beach.webp';
+
+const App = () => {
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}
+        >
+          <Text>App</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default App;
